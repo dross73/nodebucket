da061c6cd37b6e90e28b35c7b6a50d94e9174ce6
Dan <evildead73@gmail.com>
2025-04-03 17:52:13 -0500

    Fix: fully configure CORS with preflight and allowed headers

--- server/app.js
+++ server/app.js
@@ -32,9 +32,12 @@ let app = express();
 const corsOptions = {
   origin: 'https://nodebucket-front-end.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
+
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 app.use(express.json());
 app.use(express.urlencoded({ 'extended': true }));
 app.use(morgan('dev'));
